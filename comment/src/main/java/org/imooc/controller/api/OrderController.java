0ef06939edeb14d5883564a8e288c10c2585e40a
liuzhuzhu <846061407@qq.com>
2020-12-12 10:44:59 +0800

    team2 开发订单  第一天

---
+++ comment/src/main/java/org/imooc/controller/api/OrderController.java
@@ -0,0 +1,10 @@
+package org.imooc.controller.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+    public void addOrder(){
+        System.out.println("team2 开发订单  第一天");
+    }
+}
