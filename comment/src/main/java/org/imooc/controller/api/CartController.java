2044e5ebcc4944cd0994eea1096b007c7e7e4c20
liuzhuzhu <846061407@qq.com>
2021-01-12 15:56:56 +0800

    team1 开发 购物车第二天

--- comment/src/main/java/org/imooc/controller/api/CartController.java
+++ comment/src/main/java/org/imooc/controller/api/CartController.java
@@ -7,4 +7,8 @@ public class CartController {
     public void addCart(){
         System.out.println("team1 开发购物车 第一天");
     }
+
+    public void updateCart(){
+        System.out.println("team1 开发购物车 第二天");
+    }
 }
