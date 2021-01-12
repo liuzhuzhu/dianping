1309a820bf9cf4cee7c2da7ad3383dfba3eb8e6a
liuzhuzhu <846061407@qq.com>
2021-01-12 15:54:02 +0800

    team1 开发 购物车第一题

---
+++ comment/src/main/java/org/imooc/controller/api/CartController.java
@@ -0,0 +1,10 @@
+package org.imooc.controller.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CartController {
+    public void addCart(){
+        System.out.println("team1 开发购物车 第一天");
+    }
+}
